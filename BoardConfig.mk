7f05051fb7c916317f7812f0296095dc8077ca44
XiNGRZ <chenxingyu92@gmail.com>
2018-12-14 22:52:11 +0800

    Change-Id: I438399ffbd75e3754783c3c4792a28183ae81f87 ---remove cam hal shim

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,7 +36,6 @@ BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 
 # Camera
-TARGET_LD_SHIM_LIBS += /system/vendor/lib/hw/camera.msm8953.so|/system/vendor/lib/libshim_camera.so
 TARGET_LD_SHIM_LIBS += /system/vendor/lib/libmmcamera_ppeiscore.so|/system/vendor/lib/libshim_camera.so
 
 # MK Hardware

7db8a9dc9ee20b4e9fd1cc69a79f1618321d1b0f
XiNGRZ <chenxingyu92@gmail.com>
2018-12-17 13:59:21 +0800

    odin: Add releasetools
    Change-Id: I17eaf7383408e3cae6b077691c7ef5ce9a171d98

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,6 +56,7 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # Releasetools
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # inherit from the proprietary version
 -include vendor/smartisan/odin/BoardConfigVendor.mk

36f6db481ec3f8fda6cbc0a4fc1e0a67b04da36a
XiNGRZ <chenxingyu92@gmail.com>
2020-05-02 16:47:06 +0800

    odin: biometrics: Drop fingerprintd
    Change-Id: I2db19dbbae5da72ff67601736c0b2a7086994710

--- biometrics/Android.mk
+++ biometrics/Android.mk
@@ -6,22 +6,14 @@ LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.0-service.odin.rc
 LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := \
-    fingerprintd/FingerprintDaemonCallbackProxy.cpp \
-    fingerprintd/FingerprintDaemonProxy.cpp \
-    fingerprintd/IFingerprintDaemonCallback.cpp \
-    fingerprintd/IFingerprintDaemon.cpp \
-    fingerprintd/wrapper.cpp \
     BiometricsFingerprint.cpp \
     service.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libbinder \
     libutils \
     liblog \
     libhidlbase \
     libhardware \
-    libkeystore_aidl \
-    libkeystore_binder \
     android.hardware.biometrics.fingerprint@2.1
 
 include $(BUILD_EXECUTABLE)
